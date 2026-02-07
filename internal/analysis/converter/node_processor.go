8cc34af00d512dc87ee77fd9ff00ecfbe469ee61
Василёк <arina.cherednik@yandex.ru>
2026-02-07 20:08:00 +0300

    уменьшено количество go файлов

--- internal/analysis/converter/node_processor.go
+++ internal/analysis/converter/node_processor.go
@@ -1,6 +1,8 @@
 package converter
 
 import (
+	"fmt"
+	"go-ast-processor-cli/internal/models"
 	"go/token"
 	"golang.org/x/tools/go/callgraph"
 	"golang.org/x/tools/go/ssa"
@@ -31,5 +33,17 @@ func NewNodeFilter() NodeFilter {
 		}
 
 		return false
+	}
+}
+
+type NodeIdGenerator func(fn *ssa.Function, fset *token.FileSet) string
+
+func NewNodeIDGenerator() NodeIdGenerator {
+	return func(fn *ssa.Function, fset *token.FileSet) string {
+		funcPos := fset.Position(fn.Pos())
+		if fn.Pkg != nil && fn.Pkg.Pkg != nil {
+			return fmt.Sprintf(models.KeyForNodeStructure, fn.Name(), funcPos.Filename, funcPos.Line)
+		}
+		return fn.Name()
 	}
 }
